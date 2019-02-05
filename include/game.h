ad2a4c6439fea9b8f7494b94edb5937f09054389
Aaron C Gaudette <gaudetteaaron@gmail.com>
2019-02-05 15:31:37 -0500

    Add input to update callback

--- include/game.h
+++ include/game.h
@@ -1,10 +1,12 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "input.h"
+
 struct Game
 {
 	virtual void start() = 0;
-	virtual void update(double, double) = 0;
+	virtual void update(Input, double, double) = 0;
 };
 
 #endif
