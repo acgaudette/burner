e4bb579ba13a7ff1ce75dc06357dbbb329908b5e
Aaron C Gaudette <gaudetteaaron@gmail.com>
2019-02-05 15:31:29 -0500

    Add INput key handler with State

--- include/input.h
+++ include/input.h
@@ -11,4 +11,23 @@ enum Key {
 	_key_count
 };
 
+/* Thin wrapper around window library.
+ * Will be extended with additional input management later if necessary.
+ */
+struct Input
+{
+	struct State {
+		bool curr;
+		bool last;
+	};
+
+	State keys[Key::_key_count];
+
+	static int key_to_glfw(Key);
+
+	bool down(Key key);
+	bool up(Key key);
+	bool held(Key key);
+};
+
 #endif
