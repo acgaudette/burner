8421e72026ce432303cc9a98d24388083f42dccf
Aaron C Gaudette <gaudetteaaron@gmail.com>
2019-02-05 08:41:40 -0500

    Add mesh array to engine

--- include/core.h
+++ include/core.h
@@ -2,10 +2,16 @@
 #define CORE_H
 
 #include "game.h"
+#include "mesh.h"
 
+#define MAX_MESH 64
 
 struct Engine
 {
+	Mesh *meshes[MAX_MESH];
+	unsigned int objects[MAX_MESH];
+	size_t count;
+
 	Engine();
 
 	void init(Game&);
