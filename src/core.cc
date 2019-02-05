faf56e4b74f3918aa7f987c0ea0c01d626e17dab
Aaron C Gaudette <gaudetteaaron@gmail.com>
2019-02-04 23:30:55 -0500

    Add defines

--- src/core.cc
+++ src/core.cc
@@ -3,3 +3,11 @@
 #include <errno.h>
 #include <GL/gl3w.h> // Runtime function loader (core only)
 #include <GLFW/glfw3.h> // OpenGL included
+
+#define WIDTH  1280
+#define HEIGHT 720
+#define TITLE "burner"
+#define CLEAR 0.2f, 0.1f, 0.1f
+#define MAJOR 4
+#define MINOR 0
+#define LOG_BUF 1024
