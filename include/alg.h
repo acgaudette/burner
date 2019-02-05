e14d8f3ef97073940a04532c08bcaece5288def6
Aaron C Gaudette <gaudetteaaron@gmail.com>
2019-02-05 10:49:24 -0500

    Add Mat4

--- include/alg.h
+++ include/alg.h
@@ -15,4 +15,18 @@ struct Vec4
 	float dot(Vec4);
 };
 
+struct Mat4
+{
+	// Column-major
+	float values[16];
+
+	Mat4();
+	Mat4(float[16]);
+
+	static Mat4 id();
+	static Mat4 perspective(float, float, float, float);
+
+	Mat4 operator*(const Mat4&);
+};
+
 #endif
