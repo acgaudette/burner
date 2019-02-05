34e5b9fa7064e91fa8133cefd96491831e4d36a3
Aaron C Gaudette <gaudetteaaron@gmail.com>
2019-02-05 10:54:59 -0500

    Implement perspective projection matrix

--- src/alg.cc
+++ src/alg.cc
@@ -1,4 +1,7 @@
+#include <math.h>
 #include "alg.h"
+
+#define PI 3.14159265
 
 Mat4::Mat4() {}
 
@@ -18,6 +21,26 @@ Mat4 Mat4::id()
 		0, 1, 0, 0,
 		0, 0, 1, 0,
 		0, 0, 0, 1
+	};
+
+	return Mat4(values);
+}
+
+Mat4 Mat4::perspective(float fov, float aspect, float near, float far)
+{
+	float half_angle = (0.5 * fov) * PI / 180;
+	float y_scale = 1 / tan(half_angle);
+	float x_scale = y_scale / aspect;
+
+	float diff = far - near;
+	float z_scale = (near + far) / diff;
+	float z_off = (-2 * near * far) / diff;
+
+	float values[] = {
+		x_scale, 0.0, 0.0, 0.0,
+		0.0, y_scale, 0.0, 0.0,
+		0.0, 0.0, z_scale, z_off,
+		0.0, 0.0, 1.0, 0.0,
 	};
 
 	return Mat4(values);
