b520f28007be19613d9df9a1c98d18d001344fae
Aaron C Gaudette <gaudetteaaron@gmail.com>
2019-02-05 11:32:32 -0500

    Remove instance data from Mesh

--- include/mesh.h
+++ include/mesh.h
@@ -1,8 +1,6 @@
 #ifndef MESH_H
 #define MESH_H
 
-#include "alg.h"
-
 struct Mesh
 {
 	float *vertices;
@@ -10,9 +8,6 @@ struct Mesh
 
 	unsigned int *indices;
 	size_t index_count;
-
-	Mat4 model;
-	Color color;
 };
 
 #endif
