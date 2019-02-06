3be381966a5c8781eec757da1770b573f07f5202
Aaron C Gaudette <gaudetteaaron@gmail.com>
2019-02-06 00:36:32 -0500

    Return view matrix from Game

--- include/game.h
+++ include/game.h
@@ -1,12 +1,13 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "alg.h"
 #include "input.h"
 
 struct Game
 {
 	virtual void start() = 0;
-	virtual void update(Input, double, double) = 0;
+	virtual Mat4 update(Input, double, double) = 0;
 };
 
 #endif
