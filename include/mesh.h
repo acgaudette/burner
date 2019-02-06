932238cfc22370c72886c552523fe1ad5a94b5ec
Aaron C Gaudette <gaudetteaaron@gmail.com>
2019-02-05 19:21:22 -0500

    Add array constructors to Mesh

--- include/mesh.h
+++ include/mesh.h
@@ -7,9 +7,12 @@ struct Mesh
 {
 	Vertex *vertices;
 	size_t vertex_count;
-
 	unsigned int *indices;
 	size_t index_count;
+
+	Mesh();
+	Mesh(Vertex*, size_t, float*, unsigned int*, size_t);
+	Mesh(Vertex*, size_t, float*, float*, unsigned int*, size_t);
 };
 
 #endif
