671f23aa9ef8cba024b0ec27976b0f84c13ffb33
Aaron C Gaudette <gaudetteaaron@gmail.com>
2019-02-10 17:13:27 -0500

    Add typedefs for game functions
    Remove Game

--- include/game.h
+++ include/game.h
@@ -1,13 +1,14 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "engine.h"
 #include "alg.h"
 #include "input.h"
 
-struct Game
-{
-	virtual void start() = 0;
-	virtual Mat4 update(Input, double, double) = 0;
-};
+typedef void (game_start)(Engine*);
+typedef game_start* (game_start_loader)();
+
+typedef Mat4 (game_update)(Engine*, Input, double, double);
+typedef game_update* (game_update_loader)();
 
 #endif
