9fd02910f90948eba6ec3a4c8e96fc2a13ad1d93
Aaron C Gaudette <gaudetteaaron@gmail.com>
2019-02-10 22:04:36 -0500

    Move runtime Renderer fields up

--- include/render.h
+++ include/render.h
@@ -15,12 +15,13 @@ void panic();
 
 struct Renderer
 {
+	Color clear_color = Color { 0.3f, 0.2f, 0.1f };
+	float fov = 60.0f;
+
 	Mesh *meshes[MAX_MESH];
 	unsigned int objects[MAX_MESH];
 	size_t mesh_count = 0;
 
-	Color clear_color = Color { 0.3f, 0.2f, 0.1f };
-	float fov = 60.0f;
 	size_t color_loc;
 	size_t model_loc;
 	size_t proj_view_loc;
