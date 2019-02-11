be67194826e4687c26c09415619bcdc62de6664d
Aaron C Gaudette <gaudetteaaron@gmail.com>
2019-02-10 21:46:39 -0500

    Expose defines and panic; remove constructor

--- include/engine.h
+++ include/engine.h
@@ -1,33 +1,14 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
-#include "mesh.h"
-#include "instance.h"
+#define WIDTH  1280
+#define HEIGHT 720
 
-#define MAX_MESH 64
-#define MAX_ENTY 1024
-
-void load_gl_functions();
+void panic();
 
 struct Engine
 {
-	Mesh *meshes[MAX_MESH];
-	unsigned int objects[MAX_MESH];
-	size_t mesh_count;
-
-	Instance ent_data[MAX_ENTY];
-	size_t mesh_ids[MAX_ENTY];
-	size_t ent_count;
-
-	Color clear_color = Color { 0.3f, 0.2f, 0.1f };
-	float fov = 60.0f;
-
-	Engine();
-
 	void init();
-	size_t add_mesh(Mesh*, bool compute_normals = true);
-	ent add_ent(size_t);
-	void update_entity(ent, Instance);
 };
 
 #endif
