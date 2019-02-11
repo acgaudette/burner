a0250cfc73b179018eda67400045215d0c208112
Aaron C Gaudette <gaudetteaaron@gmail.com>
2019-02-10 21:46:48 -0500

    Add State and Renderer to callbacks

--- include/game.h
+++ include/game.h
@@ -1,14 +1,15 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include "engine.h"
 #include "alg.h"
+#include "state.h"
+#include "render.h"
 #include "input.h"
 
-typedef void (game_start)(Engine*);
+typedef void (game_start)(State*, Renderer*);
 typedef game_start* (game_start_loader)();
 
-typedef Mat4 (game_update)(Engine*, Input, double, double);
+typedef Mat4 (game_update)(State*, Renderer*, Input, double, double);
 typedef game_update* (game_update_loader)();
 
 #endif
